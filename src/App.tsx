b4da5ae2d5e57b8aa35aef3b356123597037824c
mori8 <kaithape@gmail.com>
2021-12-09 22:51:01 +0900

    폰트 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './main/main';
+import Header from './main/Header'
+import Footer from './main/Footer'
+import styled from 'styled-components'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  min-height: 100vh;
+`;
+
 
 export default App;
