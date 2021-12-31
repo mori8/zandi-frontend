5eb41e96f5034ef1f00e8707ab4e1ed701326d1d
mori8 <kaithape@gmail.com>
2021-12-31 23:49:15 +0900

    Design: Header 디자인 변경

--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components'
 
+
 // TODO: 회원가입 구현 이후 로그인 정보 표시
 const Header = () => {
   return (
     <StyledHeader>
+      <img src="gdsc_sookmyung_logo.png" alt="" width="280px"/> <br/>
       정원사 눈송이와 함께 <br/>
       1일 1커밋 도전하기 <br/>
       <StyledButton>이벤트 등록하기 &#10141;</StyledButton>
@@ -18,8 +20,8 @@ const StyledHeader = styled.header`
   font-family: 'Pretendard';
   font-size: 2.8rem;
   font-weight: 700;
-  background-color: #666;
-  color: white;
+  background-color: #ddd;
+  color: black;
 `;
 
 const StyledButton = styled.button`
@@ -29,9 +31,8 @@ const StyledButton = styled.button`
   padding: 0.8rem 2rem;
   border: none;
   border-radius: 1.6rem;
-  background: #d3cce3; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #d3cce3, #e9e4f0); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #d3cce3, #e9e4f0);
+  background: rgb(20, 44, 121);
+  color: white;
 `;
 
 export default Header;
