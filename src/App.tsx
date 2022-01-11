cbfe519ce14b5adb8c4f14dacb86f07ffd937c4d
mori8 <kaithape@gmail.com>
2022-01-11 23:30:02 +0900

    Update: route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import Main from './main/main';
-import Header from './main/Header'
+import EventsDetail from './eventsDetail/EventsDetail';
 import Footer from './main/Footer'
 import styled from 'styled-components'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div className="App">
-      <Wrapper>
-        <Header/>
-        <Main/>
-        <Footer/>
-      </Wrapper>
-    </div>
+    <Router>
+      <div className="App">
+        <BodyWrapper>
+          <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/:id/*" element={<EventsDetail/>}/>
+          </Routes>
+          <Footer/>
+        </BodyWrapper>
+      </div>
+    </Router>
   );
 }
 
-const Wrapper = styled.div`
+const BodyWrapper = styled.div`
   display: flex;
   min-height: 100vh;
   flex-direction: column;
   width: 68rem;
   margin: 0 auto;
 `;
-
 
 export default App;
