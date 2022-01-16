91872843814d9f799c0a58bf064653a56d16c3a8
mori8 <kaithape@gmail.com>
2022-01-17 00:31:30 +0900

    Implement: 프로필 hover 이벤트

--- src/eventsDetail/EventHeader.tsx
+++ src/eventsDetail/EventHeader.tsx
@@ -7,6 +7,33 @@ import { COLORS } from '../constants';
 const EventHeader = (props: EventInfoPropTypes) => {
   const { id, title, description, start_at, end_at, member } = props;
 
+  const showMemberInfo = (e: React.MouseEvent<HTMLImageElement>) => {
+    e.preventDefault();
+    const MemberDetailBox = document.createElement('div');
+    MemberDetailBox.innerHTML = `<p style="margin: 0 0 0.1rem; padding: 0;"}>${e.currentTarget.dataset.membername}</p><p style="margin: 0; padding: 0; font-weight: 400;">${e.currentTarget.dataset.membergithub}</p>`;
+    MemberDetailBox.setAttribute('id', 'member-detail-box');
+    MemberDetailBox.style.position = 'absolute';
+    MemberDetailBox.style.top = `${e.currentTarget.offsetTop + e.currentTarget.offsetHeight + 10}px`;
+    MemberDetailBox.style.padding = '0.6rem 1rem';
+    MemberDetailBox.style.left = `${e.currentTarget.offsetLeft}px`;
+    MemberDetailBox.style.backgroundColor = '#fff';
+    MemberDetailBox.style.boxShadow = '0 0 0.5rem 0 rgba(0,0,0,0.2)';
+    MemberDetailBox.style.borderRadius = '4px';
+    MemberDetailBox.style.fontSize = "0.8rem";
+    MemberDetailBox.style.color = '#333';
+    MemberDetailBox.style.lineHeight = '1';
+    const TargetElement = document.getElementById('member-wrapper');
+    TargetElement!.appendChild(MemberDetailBox);
+    // alert(`${e.currentTarget.dataset.membername}님의 깃허브 주소는 ${e.currentTarget.dataset.membergithub}입니다.`);
+  }
+
+  const removeMemberInfo = (e: React.MouseEvent<HTMLImageElement>) => {
+    const MemberDetailBox = document.getElementById('member-detail-box');
+    if (MemberDetailBox) {
+      MemberDetailBox.remove();
+    }
+  }
+
   return (
     <StyledEventHeader color={COLORS[id % 4]}>
       <LeftWrapper>
@@ -20,14 +47,16 @@ const EventHeader = (props: EventInfoPropTypes) => {
       </LeftWrapper>
       <RightWrapper>
         <span>참여자</span>
-        <MemberWrapper>
+        <MemberWrapper id={"member-wrapper"}>
           {member.map((member, index) => 
             <MemberProfileImg
               key={index}
               data-membername={member.name}
               data-membergithub={member.github}
               src={`https://github.com/${member.github}.png`}
-            />
+              onMouseOver={showMemberInfo}
+              onMouseLeave={removeMemberInfo}
+              />
           )}
         </MemberWrapper>
       </RightWrapper>
@@ -66,6 +95,7 @@ const MemberWrapper = styled.div`
 const MemberProfileImg = styled.img`
   width: 50px;
   border-radius: 50%;
+  position: relative;
 `;
 
 const EventTitle = styled.h1`
